961096c921d45d181bb418aeb25897c4c6bd57ef
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-03-24 16:03:59 +0000

    Added different hover effects and favicon

--- expert-matcher-ui/src/components/common.ts
+++ expert-matcher-ui/src/components/common.ts
@@ -1,2 +1,4 @@
 export const buttonStyle =
-  'text-left border border-gray-200 rounded-md p-2 hover:bg-teal-900 hover:opacity-90 hover:text-white disabled:opacity-50 disabled:cursor-not-allowed';
+  `text-left border border-gray-200 rounded-md p-2 
+    hover:scale-101 hover:text-white transition duration-300 hover:underline ease-in-out
+    disabled:opacity-50 disabled:cursor-not-allowed`;
