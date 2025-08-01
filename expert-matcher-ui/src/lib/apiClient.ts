5b436916f5fd0088090ff1862e4a3bd582cfd1b2
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-08-01 08:43:53 +0100

    Simplified dynamic profile retrieval

--- expert-matcher-ui/src/lib/apiClient.ts
+++ expert-matcher-ui/src/lib/apiClient.ts
@@ -2,10 +2,10 @@ import { getSessionId } from "./sessionFunctions";
 import { DynamicConsultantProfile } from "../types/dynamic_consultant_profile";
 
 export async function getExpertMatcherProfile(email: string): Promise<DynamicConsultantProfile> {
-    const reportUrl = window.expertMatcherConfig.reportUrl;
+    // const reportUrl = window.expertMatcherConfig.reportUrl;
     const sessionId = getSessionId();
     debugger
-    const url = `${reportUrl}/api/dynamic-profile/${sessionId}?email=${email}`;
+    const url = `/api/dynamic-profile/${sessionId}?email=${email}`;
     const response = await fetch(url);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
